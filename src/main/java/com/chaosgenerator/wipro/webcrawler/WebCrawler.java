5fd29559d1b903b07ea8acd73ae2fd79ce2c837f
chaos_generator <gutouehara@gmail.com>
2018-01-28 20:16:24 +0000

    Get title, imgs and links from page

--- src/main/java/com/chaosgenerator/wipro/webcrawler/WebCrawler.java
+++ src/main/java/com/chaosgenerator/wipro/webcrawler/WebCrawler.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -56,13 +57,14 @@ public class WebCrawler {
 
             if (doc == null) continue;
 
+            setPageTitle(currentPage, doc);
+
+            Elements imgs = doc.select("img[src]");
+            Elements links = doc.select("a[href]");
 
         }
 
         /**
-         * 9. Extract title from file
-         * 10. Extract links from file
-         * 11. Maybe consider spawning threads to process multiple pages at the same time
          * 12. Create page object with title, url and list of links.
          * 13. Add page to a map
          * 14. Add links a to toVisit list, filtering out links from other websites.
@@ -70,6 +72,11 @@ public class WebCrawler {
          * 16. maybe format the file
          */
 
+    }
+
+    void setPageTitle(Page currentPage, Document doc) {
+        String title = doc.title();
+        currentPage.setTitle(title);
     }
 
     public Document getDocument(String urlToVisit, Page currentPage, Map<Page, String> sitemap) {
